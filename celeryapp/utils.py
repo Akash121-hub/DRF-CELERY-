d33ebe3266b0aee2331aae18bbb432d36f75270a
Akash121giglabz <akash.s@giglabz.com>
2024-04-24 10:58:23 +0530

    commit

--- celeryapp/utils.py
+++ celeryapp/utils.py
@@ -36,3 +36,60 @@ class Queue:
 
     def size(self):
         return len(self.items)
+
+
+# Each node key, value, prev, next
+class ListNode:
+    def __init__(self, key=None, value=None) :
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity) -> None:
+        self.capacity = capacity
+        self.cache = {}
+        self.head = ListNode()
+        self.tail = ListNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def add_node(self,node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+    
+    def remove_node(self,node):
+        prev = node.prev
+        new = node.next
+        prev.next = new
+        new.prev = prev
+
+    def _move_to_head(self, node):
+        self._remove_node(node)
+        self._add_node(node)
+
+    def get(self, key):
+        if key in self.cache:
+            node = self.cache[key]
+            self._move_to_head(node)
+            return node.value
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._move_to_head(node)
+        else:
+            if len(self.cache) == self.capacity:
+                # Remove the least recently used node
+                tail = self.tail.prev
+                self._remove_node(tail)
+                del self.cache[tail.key]
+
+            new_node = ListNode(key, value)
+            self.cache[key] = new_node
+            self._add_node(new_node)
